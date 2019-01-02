60dbc9da6d179206da726f93a03d0d7928f79669
Vincent Vella <vincevella0@gmail.com>
2019-01-02 12:19:13 -0500

    Update package for universal scaling dependent on window size
    6 per line for now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { TouchableOpacity, View, Text, Dimensions, StyleSheet } from 'react-native';
+
+const { width } = Dimensions.get('window');
 
 export default class ColorPalette extends Component {
   static getContrastColor(hex) {
@@ -10,9 +12,7 @@ export default class ColorPalette extends Component {
   constructor(props) {
     super(props);
     const { defaultColor, value, colors } = props;
-    this.state = {
-      color: defaultColor || value || colors[0]
-    };
+    this.state = { color: defaultColor || value || colors[0] };
     this.renderIcon = this.renderIcon.bind(this);
     this.onColorChange = this.onColorChange.bind(this);
     this.renderColorOption = this.renderColorOption.bind(this);
@@ -21,13 +21,13 @@ export default class ColorPalette extends Component {
   componentWillReceiveProps(nextProps) {
     const { value } = this.props;
     if (nextProps.value !== value) {
-      this.setState({color: nextProps.value})
+      this.setState({ color: nextProps.value });
     }
   }
 
   onColorChange(color) {
     const { onChange } = this.props;
-    this.setState({color}, () => onChange(color))
+    this.setState({ color }, () => onChange(color));
   }
 
   renderIcon() {
@@ -36,24 +36,40 @@ export default class ColorPalette extends Component {
     if (icon) {
       return icon;
     }
-    return <Text style={{color: ColorPalette.getContrastColor(color), fontSize: 18}}>✔︎</Text>
+    return <Text style={{ color: ColorPalette.getContrastColor(color), fontSize: 60 }} adjustsFontSizeToFit>✔︎</Text>;
   }
 
   renderColorOption(c) {
     const { color } = this.state;
+    const { scaleToWindow } = this.props;
+    let scaledWidth = width * .025;
     return (
-      <TouchableOpacity style={[styles.colorOption, {backgroundColor: c}]} onPress={() => this.onColorChange(c)} key={c}>
+      <TouchableOpacity
+        key={c}
+        onPress={() => this.onColorChange(c)}
+        style={[
+          styles.colorOption,
+          { backgroundColor: c },
+          scaleToWindow && {
+            width: width * .07,
+            height: width * .07,
+            marginHorizontal: scaledWidth,
+            marginVertical: scaledWidth,
+            borderRadius: scaledWidth * 2
+          }
+        ]}
+      >
         {color === c && this.renderIcon()}
       </TouchableOpacity>
     );
   }
 
   render() {
-    const { title, paletteStyles, colors } = this.props;
+    const { title, paletteStyles, colors, titleStyles } = this.props;
     return (
       <View style={styles.container}>
-        <Text>{title}</Text>
-        <View style={[styles.colorContainer, {...paletteStyles}]}>
+        <Text style={[styles.titleStyles, { ...titleStyles }]}>{title}</Text>
+        <View style={[styles.colorContainer, { ...paletteStyles }]}>
           {colors.map((c) => this.renderColorOption(c))}
         </View>
       </View>
@@ -61,9 +77,13 @@ export default class ColorPalette extends Component {
   }
 }
 
+//<editor-fold desc="Default Styles">
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  titleStyles: {
+    color: 'black',
   },
   colorContainer: {
     flexDirection: 'row',
@@ -82,23 +102,26 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     borderRadius: 15,
     elevation: 5,
-    shadowOffset: {width: 2, height: 2},
+    shadowOffset: { width: 2, height: 2 },
     shadowColor: 'black',
     shadowOpacity: .25,
   }
 });
+//</editor-fold>
 
 ColorPalette.defaultProps = {
+  value: null,
+  titleStyles: {},
+  paletteStyles: {},
+  onChange: () => {},
+  defaultColor: null,
+  scaleToWindow: false,
+  title: "Color Palette:",
   colors: [
     '#C0392B', '#E74C3C', '#9B59B6', '#8E44AD', '#2980B9', '#3498DB', '#1ABC9C',
     '#16A085', '#27AE60', '#2ECC71', '#F1C40F', '#F39C12', '#E67E22', '#D35400',
     '#ffffff', '#BDC3C7', '#95A5A6', '#7F8C8D', '#34495E', '#2C3E50', '#000000',
   ],
-  defaultColor: null,
-  value: null,
-  title: "Color Palette:",
-  onChange: () => {},
-  paletteStyles: {}
 };
 
 ColorPalette.propTypes = {
